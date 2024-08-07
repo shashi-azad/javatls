d7002e40288e66b69e090a6e2e73301ef684a4da
Shashi Azad <Shashi.Azad@dell.com>
2024-08-07 14:58:57 +0530

    first commit

---
+++ src/main/java/com/example/javatls/JavatlsApplication.java
@@ -0,0 +1,13 @@
+package com.example.javatls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavatlsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavatlsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javatls/JavatlsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javatls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavatlsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
